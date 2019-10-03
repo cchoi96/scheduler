8c488757e52ecb6dc1ac9f960c6d51536606f60e
Chris Choi <chrischoi96@gmail.com>
2019-10-03 17:03:47 -0400

    fixed spots functionality so it renders correctly on book/cancel interview

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,28 +1,86 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import axios from "axios";
 
 const useApplicationData = () => {
-  const [state, setState] = useState({
+  const SET_DAY = "SET_DAY";
+  const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+  const SET_INTERVIEW = "SET_INTERIVEW";
+  const SET_SPOT_COUNT = "SET_COUNT";
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case SET_DAY:
+        return {
+          ...state,
+          day: action.value
+        };
+      case SET_APPLICATION_DATA:
+        return {
+          ...state,
+          days: action.value[0].data,
+          appointments: action.value[1].data,
+          interviewers: action.value[2].data
+        };
+      case SET_INTERVIEW:
+        return {
+          ...state,
+          appointments: action.value
+        };
+      case SET_SPOT_COUNT:
+        return {
+          ...state,
+          days: action.value
+        };
+      default:
+        throw new Error(
+          `Tried to reduce with unsupported action type: ${action.type}`
+        );
+    }
+  };
+
+  const updateObjectInArray = (array, action) => {
+    return array.map((item, index) => {
+      if (index !== action.index) {
+        return item;
+      }
+      return {
+        ...item,
+        spots: action.item
+      };
+    });
+  };
+
+  const getDayId = appointmentId => {
+    let dayId = 0;
+    if (appointmentId > 20) {
+      dayId = 4;
+    } else if (appointmentId > 15) {
+      dayId = 3;
+    } else if (appointmentId > 10) {
+      dayId = 2;
+    } else if (appointmentId > 5) {
+      dayId = 1;
+    }
+    console.log(appointmentId);
+    return dayId;
+  };
+
+  const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
     appointments: {},
     interviewers: {}
   });
 
-  const setDay = day => setState({ ...state, day });
+  const setDay = day => dispatch({ type: SET_DAY, value: day });
 
   useEffect(() => {
     Promise.all([
       axios.get("/api/days"),
       axios.get("/api/appointments"),
       axios.get("/api/interviewers")
-    ]).then(([days, appointments, interviewers]) => {
-      setState(prev => ({
-        ...prev,
-        days: days.data,
-        appointments: appointments.data,
-        interviewers: interviewers.data
-      }));
+    ]).then(all => {
+      dispatch({ type: SET_APPLICATION_DATA, value: all });
     });
   }, []);
 
@@ -35,11 +93,19 @@ const useApplicationData = () => {
             ...state.appointments[id],
             interview: { ...interview }
           };
+          console.log("State: ", state);
           const appointments = {
             ...state.appointments,
             [id]: appointment
           };
-          setState({ ...state, appointments });
+          let dayId = getDayId(id);
+          let days = updateObjectInArray(state.days, {
+            index: dayId,
+            item: state.days[dayId].spots - 1
+          });
+
+          dispatch({ type: SET_SPOT_COUNT, value: days });
+          dispatch({ type: SET_INTERVIEW, value: appointments });
         }
       });
   };
@@ -55,7 +121,13 @@ const useApplicationData = () => {
           ...state.appointments,
           [id]: appointment
         };
-        setState({ ...state, appointments });
+        let dayId = getDayId(id);
+        let days = updateObjectInArray(state.days, {
+          index: dayId,
+          item: state.days[dayId].spots + 1
+        });
+        dispatch({ type: SET_SPOT_COUNT, value: days });
+        dispatch({ type: SET_INTERVIEW, value: appointments });
       }
     });
   };
