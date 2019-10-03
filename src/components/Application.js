f67dae78615f6ba5202a7ef63f63035875082e59
Chris Choi <chrischoi96@gmail.com>
2019-10-03 14:13:28 -0400

    Moved state handlers to separate file

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
+import useApplicationData from "hooks/useApplicationData";
 import {
   getAppointmentsForDay,
   getInterview,
@@ -10,67 +11,14 @@ import {
 import axios from "axios";
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-
-  const setDay = day => setState({ ...state, day });
-
-  const getDaysData = axios.get("/api/days");
-  const getAppointmentData = axios.get("/api/appointments");
-  const getInterviewersData = axios.get("/api/interviewers");
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
   const interviewers = getInterviewersForDay(state, state.day);
-
-  useEffect(() => {
-    Promise.all([getDaysData, getAppointmentData, getInterviewersData]).then(
-      all => {
-        const [days, appointments, interviewers] = all;
-        setState(prev => ({
-          days: days.data,
-          appointments: appointments.data,
-          interviewers: interviewers.data
-        }));
-      }
-    );
-  }, []);
-
-  const bookInterview = (id, interview) => {
-    return axios
-      .put(`/api/appointments/${id}`, { interview })
-      .then(response => {
-        if (response.status >= 200 && response.status < 300) {
-          const appointment = {
-            ...state.appointments[id],
-            interview: { ...interview }
-          };
-          const appointments = {
-            ...state.appointments,
-            [id]: appointment
-          };
-          setState({ ...state, appointments });
-        }
-      });
-  };
-
-  const cancelInterview = id => {
-    return axios.delete(`/api/appointments/${id}`).then(response => {
-      if (response.status >= 200 && response.status < 300) {
-        const appointment = {
-          ...state.appointments[id],
-          interview: null
-        };
-        const appointments = {
-          ...state.appointments,
-          [id]: appointment
-        };
-        setState({ ...state, appointments });
-      }
-    });
-  };
 
   const scheduleData = getAppointmentsForDay(state, state.day).map(
     appointment => {
